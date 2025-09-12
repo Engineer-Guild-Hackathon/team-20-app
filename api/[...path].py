0b60ccfb7358c4be7de463ba2ed09c6587e02b8e
GudeGudeEgg <ok230116@oks.iput.ac.jp>
2025-09-13 03:41:59 +0900

    fileを__file__に変更

--- api/[...path].py
+++ api/[...path].py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
     server_path = os.path.join(project_root, 'server')
