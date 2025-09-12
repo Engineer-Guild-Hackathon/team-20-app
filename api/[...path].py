f680c389c1acc3ecbaf023393ce2c27eaa8c46ed
GudeGudeEgg <ok230116@oks.iput.ac.jp>
2025-09-13 03:57:11 +0900

    change

--- api/[...path].py
+++ api/[...path].py
@@ -4,8 +4,9 @@ import os
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
+
 server_path = os.path.join(project_root, 'server')
 if server_path not in sys.path:
     sys.path.insert(0, server_path)
 
-from server.main import app  # ASGI app
+from server.main import app as app
